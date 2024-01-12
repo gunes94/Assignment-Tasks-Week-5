2d2355a2eb498e726130b5922ff1f8c4be8b1c84
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 18:20:06 +0200

    Create StudentTest.java

---
+++ StudentTest.java
@@ -0,0 +1,13 @@
+public class StudentTest {
+
+  public static void main(String[] args) {
+
+    Student student1 = new Student("Петров", "Иван", 12, 6);
+    Student student2 = new Student("Хаджиева", "Теодора", 15, 9);
+    Student student3 = new Student("Енчев", "Емил", 18, 12);
+
+    System.out.println(student1);
+    System.out.println(student2);
+    System.out.println(student3);
+  }
+}
